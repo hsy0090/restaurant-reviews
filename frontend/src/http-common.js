974cbd019c08a8696b731d0f3a991860ca441352
Hsy0090 <hsy0090.hsy@gmail.com>
2026-02-25 21:41:08 +0800

    Missed files

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/v1/restaurants",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1/restaurants",
   headers: {
     "Content-type": "application/json"
   }
